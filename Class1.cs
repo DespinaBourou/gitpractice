a573c1f4b45f08d1b270f58657cef282a33af63a
DespinaBourou <despina.bourou@intragen.com>
2024-12-27 16:45:13 +0200

    added 2 comments on branch1

--- Class1.cs
+++ Class1.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 namespace gitpractice
 {
     public class Class1
-    {
+    {//test branch1 comment outside
         public void writeMessage()
-        {
+        {//test branch1 comment inside
             Console.WriteLine("hello there");
 
         }
